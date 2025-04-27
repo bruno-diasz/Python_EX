24325ca6584507e78b47cd12b15e7b5e1f81ee62
Bruno <brunoipb1@gmail.com>
2025-04-27 23:03:28 +0000

    Fist Commit

---
+++ Circulo.py
@@ -0,0 +1,29 @@
+import math
+
+class Circulo:
+    def __init__(self):
+        self.__raio = 0
+        self.__altura = 0
+
+    @property
+    def raio(self) -> float:
+        return self.__raio
+    @raio.setter
+    def raio(self,valor: int):
+        if valor > 0: self.__raio = valor
+        else: raise ValueError("O valor deve ser positivo")
+        
+    
+    def calc_area(self):
+        area = math.pi * self.__raio ** 2
+        return area  
+    def calc_circunferencia(self):
+        circunferencia = 2 * math.pi * self.__raio 
+        return circunferencia
+
+circulo= Circulo()
+circulo.raio = int(input("digite um numero: "))
+print(circulo.raio)
+print (circulo.calc_area())
+
+
