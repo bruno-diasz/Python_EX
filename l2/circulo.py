f2a0673779d94bac17318a2b5a8e582a624e8877
Bruno <brunoipb1@gmail.com>
2025-04-30 17:21:21 +0000

    Iniciando lista 3

--- l2/circulo.py
+++ l2/circulo.py
@@ -3,27 +3,32 @@ import math
 class Circulo:
     def __init__(self):
         self.__raio = 0
-        self.__altura = 0
+        self.__altura = 'oi'
 
+    #=== Getter e Setter ===
     @property
     def raio(self) -> float:
         return self.__raio
+    
     @raio.setter
     def raio(self,valor: int):
         if valor > 0: self.__raio = valor
         else: raise ValueError("O valor deve ser positivo")
-        
-    
+
+    #=== Metodos da classe ===
     def calc_area(self) -> float:
         area = math.pi * self.__raio ** 2
         return area  
-    def calc_circunferencia(self):
+    
+    def calc_circunferencia(self) -> float:
         circunferencia = 2 * math.pi * self.__raio 
         return circunferencia
+    
+    def __str__(self):
+        return f'Seu raio é: {self.__raio}'
 
 circulo= Circulo()
 circulo.raio = int(input("digite um numero: "))
-print(circulo.raio)
-print (circulo.calc_area())
+print (circulo)
 
 
---
+++ l3/retangulo.py
@@ -0,0 +1,39 @@
+import math
+class Retangulo():
+    def __init__(self, b:int, h:int):
+        self.b = b
+        self.h = h
+    #=== Getter e setter de B ===
+    @property
+    def b(self):
+        return self.__b
+    @b.setter
+    def b(self,b: int):
+        if b > 0:
+            self.__b = b
+        else:
+            raise ValueError("Valor precisa ser positivo") 
+        
+     #=== Getter e setter de H ===
+    @property
+    def h(self):
+        return self.__h
+    @h.setter
+    def h(self,h: int):
+        if h > 0:
+            self.__h = h
+        else:
+            raise ValueError("Valor precisa ser positivo") 
+    
+    #=== Metodos da Classe ===
+    def CalcArea(self):
+        return self.b*self.h
+    
+    def CalcDiagonal(self):
+        return math.sqrt(self.b**2+self.h**2)
+    
+    def __str__(self):
+        return f"Base = {self.b}\nAltura = {self.h}\nArea = {self.CalcArea()}\nDiagonal = {self.CalcDiagonal()}"
+    
+a= Retangulo(3,5)
+print(a)
